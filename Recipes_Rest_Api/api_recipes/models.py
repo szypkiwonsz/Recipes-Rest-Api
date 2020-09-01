1ed581c2caf4fd3100f9a19a554b0ee73840b158
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-01 20:37:04 +0200

    Make corrections in code
    - delete unique=True in name Field -> Food model
    - change name of field from name to food -> Ingredient model
    - change __str__ return in Ingredient model
    - fix typo in preparation_description field -> Recipe model

--- Recipes_Rest_Api/api_recipes/models.py
+++ Recipes_Rest_Api/api_recipes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
@@ -14,12 +14,12 @@ class Ingredient(models.Model):
         ('PIECE', 'PIECE'),
         ('GRAM', 'GRAM')
     )
-    name = models.ForeignKey(Food, on_delete=models.PROTECT)
+    food = models.ForeignKey(Food, on_delete=models.PROTECT)
     unit = models.CharField(max_length=5, choices=UNIT_CHOICES)
     amount = models.PositiveIntegerField(blank=True, default=0)
 
     def __str__(self):
-        return f'{self.name}, {self.amount}'
+        return f'{self.food.name}, {self.amount}'
 
 
 class Recipe(models.Model):
@@ -41,7 +41,7 @@ class Recipe(models.Model):
     preparation_time = models.PositiveIntegerField(help_text='Time in minutes.')
     difficulty = models.CharField(max_length=6, choices=DIFFICULTY_CHOICES)
     rating = models.CharField(max_length=1, choices=RATING_CHOICES, blank=True, default=0)
-    preparation_description = models.Textield()
+    preparation_description = models.TextField()
 
     def __str__(self):
         return self.name
