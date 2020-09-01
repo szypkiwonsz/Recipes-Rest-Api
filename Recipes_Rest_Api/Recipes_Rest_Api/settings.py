2cf59e7df0d36b4182b9ca0cdff7720b207ccb72
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-01 17:59:31 +0200

    Add rest_framework to installed apps

--- Recipes_Rest_Api/Recipes_Rest_Api/settings.py
+++ Recipes_Rest_Api/Recipes_Rest_Api/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_recipes.apps.ApiRecipesConfig'
+    'api_recipes.apps.ApiRecipesConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
