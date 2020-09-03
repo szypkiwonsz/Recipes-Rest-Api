c4a435c14d435ee6905807f3cbe7741626038f93
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-03 19:18:45 +0200

    Improve code formatting in update method

--- Recipes_Rest_Api/api_recipes/serializers.py
+++ Recipes_Rest_Api/api_recipes/serializers.py
@@ -25,6 +25,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        """
+        Method for writable nested serializer
+        """
         ingredients_data = validated_data.pop('ingredients')
         recipe = Recipe.objects.create(**validated_data)
         for ingredient_data in ingredients_data:
@@ -37,32 +40,26 @@ class RecipeSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, instance, validated_data):
-        try:
-            ingredients_data = validated_data.pop('ingredients')
-        except KeyError:
-            ingredients_data = []
+        """
+        Method for writable nested serializer
+        """
         instance = super(RecipeSerializer, self).update(instance, validated_data)
+        ingredients_data = validated_data.pop('ingredients', [])
         for ingredient_data in ingredients_data:
-            foods_data = [ingredient_data.pop('food')]
+            # foods_data must be a list, because there is one field in the database
+            foods_data = [ingredient_data.pop('food', [])]
             for food_data in foods_data:
-                food_qs = Food.objects.filter(
-                    name__iexact=food_data['name']
-                )
-
+                food_qs = Food.objects.filter(name__iexact=food_data['name'])
                 if food_qs.exists():
                     food = food_qs.first()
                 else:
                     food = Food.objects.create(**food_data)
 
-                ingredient_qs = Ingredient.objects.filter(
-                    unit__iexact=ingredient_data['unit']).filter(
+                ingredient_qs = Ingredient.objects.filter(unit__iexact=ingredient_data['unit']).filter(
                     amount__iexact=ingredient_data['amount']).filter(food_id=food.pk)
-
                 if ingredient_qs.exists():
                     ingredient = ingredient_qs.first()
                 else:
                     ingredient = Ingredient.objects.create(food_id=food.pk, **ingredient_data)
-
                 instance.ingredients.add(ingredient)
-
         return instance
