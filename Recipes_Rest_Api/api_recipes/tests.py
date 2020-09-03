054f1dbf74079566726afbe4d6748cfb0e914a8d
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-03 20:21:32 +0200

    Add TestRecipeViewSet class
    Add tests for recipe list (authorized, unauthorized) -> GET

--- Recipes_Rest_Api/api_recipes/tests.py
+++ Recipes_Rest_Api/api_recipes/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
 
 # Create your tests here.
+class TestRecipeViewSet(APITestCase):
+    list_url = reverse('recipe-list')
+
+    def setUp(self):
+        self.user = User.objects.create(username='username', password='password')
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='TOKEN ' + self.token.key)
+
+    def test_recipe_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_recipe_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
