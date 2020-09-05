cddd4b826f3f15af7b59ad61a086e596aee93910
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-05 22:12:53 +0200

    Fix code in tests

--- Recipes_Rest_Api/api_recipes/tests.py
+++ Recipes_Rest_Api/api_recipes/tests.py
@@ -26,12 +26,18 @@ class TestRecipeViewSet(APITestCase):
                     "amount": 1
                 }
             ],
+            "steps": [
+                {
+                    "instruction": "Dice the tomato.",
+                    "order": 1
+                }
+            ],
             "name": "Salad",
             "portions": 1,
             "preparation_time": 1,
             "difficulty": "EASY",
             "rating": "",
-            "preparation_description": "Dice the tomato."
+            "description": "Nice recipe."
         }
         self.recipes_created = [{
             "id": 1,
@@ -40,10 +46,18 @@ class TestRecipeViewSet(APITestCase):
                     "id": 1,
                     "food": {
                         "id": 1,
-                        "name": "Tomato"
+                        "name": "Tomato",
+                        "recipe": None
                     },
                     "unit": "PIECE",
                     "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the tomato.",
+                    "order": 1
                 }
             ],
             "name": "Salad",
@@ -51,7 +65,7 @@ class TestRecipeViewSet(APITestCase):
             "preparation_time": 1,
             "difficulty": "EASY",
             "rating": "",
-            "preparation_description": "Dice the tomato."
+            "description": "Nice recipe."
         }]
         self.recipe_created = {
             "id": 1,
@@ -60,10 +74,18 @@ class TestRecipeViewSet(APITestCase):
                     "id": 1,
                     "food": {
                         "id": 1,
-                        "name": "Tomato"
+                        "name": "Tomato",
+                        "recipe": None
                     },
                     "unit": "PIECE",
                     "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the tomato.",
+                    "order": 1
                 }
             ],
             "name": "Salad",
@@ -71,7 +93,7 @@ class TestRecipeViewSet(APITestCase):
             "preparation_time": 1,
             "difficulty": "EASY",
             "rating": "",
-            "preparation_description": "Dice the tomato."
+            "description": "Nice recipe."
         }
 
     def post_recipe(self):
@@ -113,6 +135,9 @@ class TestRecipeViewSet(APITestCase):
             "ingredients": [
                 "This field is required."
             ],
+            "steps": [
+                "This field is required."
+            ],
             "name": [
                 "This field is required."
             ],
@@ -125,32 +150,11 @@ class TestRecipeViewSet(APITestCase):
             "difficulty": [
                 "This field is required."
             ],
-            "preparation_description": [
+            "description": [
                 "This field is required."
             ]
         }
         response = self.client.post(self.list_url, format='json', data=recipe_no_data)
-        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEquals(json.loads(response.content), response_message)
-
-    def test_recipe_list_post_authenticated_no_ingredients_data(self):
-        recipe_no_ingredients = {
-            "ingredients": [],
-            "name": "Salad",
-            "portions": 1,
-            "preparation_time": 1,
-            "difficulty": "EASY",
-            "rating": "",
-            "preparation_description": "Dice the tomato."
-        }
-        response_message = {
-            "ingredients": {
-                "empty_list_error": [
-                    "Expected at least one item in list."
-                ]
-            }
-        }
-        response = self.client.post(self.list_url, format='json', data=recipe_no_ingredients)
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEquals(json.loads(response.content), response_message)
 
@@ -163,12 +167,18 @@ class TestRecipeViewSet(APITestCase):
                     "amount": 1
                 }
             ],
+            "steps": [
+                {
+                    "instruction": "Dice the tomato.",
+                    "order": 1
+                }
+            ],
             "name": "Salad",
             "portions": 1,
             "preparation_time": 1,
             "difficulty": "EASY",
             "rating": "",
-            "preparation_description": "Dice the tomato."
+            "description": "Nice recipe."
         }
         response_message = {
             "ingredients": [
@@ -183,4 +193,360 @@ class TestRecipeViewSet(APITestCase):
         }
         response = self.client.post(self.list_url, format='json', data=recipe_no_foods)
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(json.loads(response.content), response_message)
+
+    def test_recipe_detail_get_authenticated(self):
+        self.post_recipe()
+        response = self.client.get(self.detail_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), self.recipe_created)
+
+    def test_recipe_detail_get_un_authenticated(self):
+        self.post_recipe()
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.detail_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), self.recipe_created)
+
+    def test_recipe_detail_patch_authenticated_no_data(self):
+        self.post_recipe()
+        recipe_no_data = {}
+        response = self.client.patch(self.detail_url, format='json', data=recipe_no_data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), self.recipe_created)
+
+    def test_recipe_detail_patch_authenticated_name(self):
+        self.post_recipe()
+        recipe_one_field = {
+            "name": "New Salad"
+        }
+        recipe_new_name = {
+            "id": 1,
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Tomato",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the tomato.",
+                    "order": 1
+                }
+            ],
+            "name": "New Salad",
+            "portions": 1,
+            "preparation_time": 1,
+            "difficulty": "EASY",
+            "rating": "",
+            "description": "Nice recipe."
+        }
+        response = self.client.patch(self.detail_url, format='json', data=recipe_one_field)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), recipe_new_name)
+
+    def test_recipe_detail_patch_authenticated_ingredient(self):
+        self.post_recipe()
+        ingredient_new_name = {
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Strawberry",
+                        "recipe": None
+                    },
+                    "unit": "GRAM",
+                    "amount": 1
+                }
+            ],
+        }
+        new_recipe = {
+            "id": 1,
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Strawberry",
+                        "recipe": None
+                    },
+                    "unit": "GRAM",
+                    "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the tomato.",
+                    "order": 1
+                }
+            ],
+            "name": "Salad",
+            "portions": 1,
+            "preparation_time": 1,
+            "difficulty": "EASY",
+            "rating": "",
+            "description": "Nice recipe."
+        }
+        response = self.client.patch(self.detail_url, format='json', data=ingredient_new_name)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), new_recipe)
+
+    def test_recipe_detail_patch_authenticated_new_ingredient(self):
+        self.post_recipe()
+        ingredient_new = {
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Strawberry",
+                        "recipe": None
+                    },
+                    "unit": "GRAM",
+                    "amount": 1
+                },
+                {
+                    "food": {
+                        "name": "Salad",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 2
+                }
+            ],
+        }
+        new_recipe = {
+            "id": 1,
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Strawberry",
+                        "recipe": None
+                    },
+                    "unit": "GRAM",
+                    "amount": 1
+                },
+                {
+                    "id": 2,
+                    "food": {
+                        "id": 2,
+                        "name": "Salad",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 2
+
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the tomato.",
+                    "order": 1
+                }
+            ],
+            "name": "Salad",
+            "portions": 1,
+            "preparation_time": 1,
+            "difficulty": "EASY",
+            "rating": "",
+            "description": "Nice recipe."
+        }
+        response = self.client.patch(self.detail_url, format='json', data=ingredient_new)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), new_recipe)
+
+    def test_recipe_detail_patch_authenticated_food(self):
+        self.post_recipe()
+        recipe_one_field = {
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Tomato",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 1
+                }
+            ],
+        }
+        ingredient_new_name = {
+            "id": 1,
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Tomato",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the tomato.",
+                    "order": 1
+                }
+            ],
+            "name": "Salad",
+            "portions": 1,
+            "preparation_time": 1,
+            "difficulty": "EASY",
+            "rating": "",
+            "description": "Nice recipe."
+        }
+        response = self.client.patch(self.detail_url, format='json', data=recipe_one_field)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), ingredient_new_name)
+
+    def test_recipe_detail_patch_authenticated_steps(self):
+        self.post_recipe()
+        step_new_name = {
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the strawberry.",
+                    "order": 1
+                }
+            ],
+        }
+        new_recipe = {
+            "id": 1,
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Tomato",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the strawberry.",
+                    "order": 1
+                }
+            ],
+            "name": "Salad",
+            "portions": 1,
+            "preparation_time": 1,
+            "difficulty": "EASY",
+            "rating": "",
+            "description": "Nice recipe."
+        }
+        response = self.client.patch(self.detail_url, format='json', data=step_new_name)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), new_recipe)
+
+    def test_recipe_detail_patch_authenticated_new_step(self):
+        self.post_recipe()
+        step_new = {
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the strawberry.",
+                    "order": 1
+                },
+                {
+                    "instruction": "Dice the tomato.",
+                    "order": 2
+                }
+            ],
+        }
+        new_recipe = {
+            "id": 1,
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Tomato",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 1
+                }
+            ],
+            "steps": [
+                {
+                    "id": 1,
+                    "instruction": "Dice the strawberry.",
+                    "order": 1
+                },
+                {
+                    "id": 2,
+                    "instruction": "Dice the tomato.",
+                    "order": 2
+                }
+            ],
+            "name": "Salad",
+            "portions": 1,
+            "preparation_time": 1,
+            "difficulty": "EASY",
+            "rating": "",
+            "description": "Nice recipe."
+        }
+        response = self.client.patch(self.detail_url, format='json', data=step_new)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(json.loads(response.content), new_recipe)
+
+    def test_recipe_detail_patch_un_authenticated(self):
+        self.post_recipe()
+        self.client.force_authenticate(user=None)
+        recipe_one_field = {
+            "ingredients": [
+                {
+                    "id": 1,
+                    "food": {
+                        "id": 1,
+                        "name": "Tomato",
+                        "recipe": None
+                    },
+                    "unit": "PIECE",
+                    "amount": 1
+                }
+            ],
+        }
+        response_message = {
+            'detail': 'Authentication credentials were not provided.'
+        }
+        response = self.client.patch(self.detail_url, format='json', data=recipe_one_field)
+        self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEquals(json.loads(response.content), response_message)
+
+    def test_recipe_detail_delete_authenticated(self):
+        response_message = {
+            'detail': 'Not found.'
+        }
+        response = self.client.delete(self.detail_url, format='json')
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEquals(json.loads(response.content), response_message)
+
+    def test_recipe_detail_delete_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response_message = {
+            'detail': 'Authentication credentials were not provided.'
+        }
+        response = self.client.delete(self.detail_url, format='json')
+        self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEquals(json.loads(response.content), response_message)
