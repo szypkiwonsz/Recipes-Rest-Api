afcb9053b2cc3d47093c49bb7bba599aa3b307e2
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-05 22:09:54 +0200

    Register new routers
    -for FoodViewSet, IngredientViewSet, StepViewSet

--- Recipes_Rest_Api/api_recipes/router.py
+++ Recipes_Rest_Api/api_recipes/router.py
@@ -1,6 +1,9 @@
 from rest_framework import routers
 
-from .views import RecipeViewSet
+from .views import RecipeViewSet, FoodViewSet, StepViewSet, IngredientViewSet
 
 router = routers.DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipe')
+router.register('foods', FoodViewSet, basename='food')
+router.register('ingredients', IngredientViewSet, basename='ingredient')
+router.register('steps', StepViewSet, basename='steep')
