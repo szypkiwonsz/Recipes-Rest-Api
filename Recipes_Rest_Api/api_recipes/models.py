755e0489878d7208a6f481fe437502c089bd05b3
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-05 22:09:14 +0200

    Fix code
    - add recipe foreign key in Food model,
    - add Step model,
    - change fields in Recipe model

--- Recipes_Rest_Api/api_recipes/models.py
+++ Recipes_Rest_Api/api_recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length=20)
+    recipe = models.ForeignKey('Recipe', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
@@ -22,6 +23,17 @@ class Ingredient(models.Model):
         return f'{self.food.name}, {self.amount}'
 
 
+class Step(models.Model):
+    instruction = models.TextField()
+    order = models.IntegerField()
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return
+
+
 class Recipe(models.Model):
     DIFFICULTY_CHOICES = (
         ('EASY', 'EASY'),
@@ -36,12 +48,13 @@ class Recipe(models.Model):
         ('5', '5')
     )
     name = models.CharField(max_length=100)
+    description = models.TextField()
     ingredients = models.ManyToManyField(Ingredient)
     portions = models.PositiveIntegerField()
     preparation_time = models.PositiveIntegerField(help_text='Time in minutes.')
     difficulty = models.CharField(max_length=6, choices=DIFFICULTY_CHOICES)
     rating = models.CharField(max_length=1, choices=RATING_CHOICES, blank=True, default=0)
-    preparation_description = models.TextField()
+    steps = models.ManyToManyField(Step)
 
     def __str__(self):
         return self.name
