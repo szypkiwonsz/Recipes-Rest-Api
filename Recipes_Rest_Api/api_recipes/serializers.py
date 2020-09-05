8ebcb07fa66a682310dd32930404dd96d28757f3
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-05 22:11:17 +0200

    Code corrections by importing the "drg_writable_nested" library

--- Recipes_Rest_Api/api_recipes/serializers.py
+++ Recipes_Rest_Api/api_recipes/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer, UniqueFieldsMixin
 
-from .models import Recipe, Ingredient, Food
+from .models import Recipe, Ingredient, Food, Step
 
 
-class FoodSerializer(serializers.ModelSerializer):
+class FoodSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        obj, created = Food.objects.get_or_create(**validated_data)
+        return obj
+
+    def update(self, instance, validated_data):
+        return super(FoodSerializer, self).update(instance, validated_data)
+
     class Meta:
         model = Food
         fields = '__all__'
 
 
-class IngredientSerializer(serializers.ModelSerializer):
+class StepSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
+
+    class Meta:
+        model = Step
+        fields = '__all__'
+
+
+class IngredientSerializer(WritableNestedModelSerializer):
     food = FoodSerializer()
 
     class Meta:
@@ -17,49 +33,10 @@ class IngredientSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class RecipeSerializer(serializers.ModelSerializer):
+class RecipeSerializer(WritableNestedModelSerializer):
     ingredients = IngredientSerializer(many=True)
+    steps = StepSerializer(many=True)
 
     class Meta:
         model = Recipe
         fields = '__all__'
-
-    def create(self, validated_data):
-        """
-        Method for writable nested serializer
-        """
-        ingredients_data = validated_data.pop('ingredients')
-        recipe = Recipe.objects.create(**validated_data)
-        for ingredient_data in ingredients_data:
-            # foods_data must be a list, because there is one field in the database
-            foods_data = [ingredient_data.pop('food')]
-            for food_data in foods_data:
-                food, _ = Food.objects.get_or_create(**food_data)
-                ingredient_data, _ = Ingredient.objects.get_or_create(food_id=food.pk, **ingredient_data)
-                recipe.ingredients.add(ingredient_data)
-        return recipe
-
-    def update(self, instance, validated_data):
-        """
-        Method for writable nested serializer
-        """
-        instance = super(RecipeSerializer, self).update(instance, validated_data)
-        ingredients_data = validated_data.pop('ingredients', [])
-        for ingredient_data in ingredients_data:
-            # foods_data must be a list, because there is one field in the database
-            foods_data = [ingredient_data.pop('food', [])]
-            for food_data in foods_data:
-                food_qs = Food.objects.filter(name__iexact=food_data['name'])
-                if food_qs.exists():
-                    food = food_qs.first()
-                else:
-                    food = Food.objects.create(**food_data)
-
-                ingredient_qs = Ingredient.objects.filter(unit__iexact=ingredient_data['unit']).filter(
-                    amount__iexact=ingredient_data['amount']).filter(food_id=food.pk)
-                if ingredient_qs.exists():
-                    ingredient = ingredient_qs.first()
-                else:
-                    ingredient = Ingredient.objects.create(food_id=food.pk, **ingredient_data)
-                instance.ingredients.add(ingredient)
-        return instance
