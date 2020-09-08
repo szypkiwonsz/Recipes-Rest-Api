f152db65d6f8c3e378dcbf720286cdc8eddf80f8
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-08 20:41:03 +0200

    Add urlpatterns for media

--- Recipes_Rest_Api/urls.py
+++ Recipes_Rest_Api/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
+from Recipes_Rest_Api import settings
 from api_recipes.router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    ]
