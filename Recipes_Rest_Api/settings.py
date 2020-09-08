93a151eebf5f29abf10bcc8f34cb38eb92ae55a5
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-08 20:41:48 +0200

    Add MEDIA_ROOT and MEDIA_URL

--- Recipes_Rest_Api/settings.py
+++ Recipes_Rest_Api/settings.py
@@ -116,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/media')
+MEDIA_URL = '/media/'
