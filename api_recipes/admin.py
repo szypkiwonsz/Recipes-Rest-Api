b6c1dfeae7ea5f1242c3b32278f3c03a63dcdf3d
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-09 22:11:59 +0200

    Add Step model to admin site

--- api_recipes/admin.py
+++ api_recipes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from api_recipes.models import Recipe, Ingredient, Food
+from api_recipes.models import Recipe, Ingredient, Food, Step
 
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(Food)
+admin.site.register(Step)
