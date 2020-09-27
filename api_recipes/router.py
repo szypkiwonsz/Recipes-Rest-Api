3bc6eb1d6d531517d1e084077f94042baa75fbe5
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-27 22:48:07 +0200

    Rename file to urls.py

--- api_recipes/router.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from .views import RecipeViewSet, FoodViewSet, StepViewSet, IngredientViewSet
-
-router = routers.DefaultRouter()
-router.register('recipes', RecipeViewSet, basename='recipe')
-router.register('foods', FoodViewSet, basename='food')
-router.register('ingredients', IngredientViewSet, basename='ingredient')
-router.register('steps', StepViewSet, basename='steep')
