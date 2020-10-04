6a261e5ebef4703a7fcb99192fba6ec37c6a433a
szypkiwonsz <kacpersawicki321@gmail.com>
2020-10-04 14:07:19 +0200

    Comment not needed routers

--- api_recipes/urls.py
+++ api_recipes/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import ObtainAuthToken
 
-from api_recipes.views import RecipeViewSet, FoodViewSet, StepViewSet, IngredientViewSet, UserViewSet
+from api_recipes.views import RecipeViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipe')
-router.register('foods', FoodViewSet, basename='food')
-router.register('ingredients', IngredientViewSet, basename='ingredient')
-router.register('steps', StepViewSet, basename='steep')
+# router.register('foods', FoodViewSet, basename='food')
+# router.register('ingredients', IngredientViewSet, basename='ingredient')
+# router.register('steps', StepViewSet, basename='steep')
 router.register('users', UserViewSet, basename='user')
 
 urlpatterns = [
